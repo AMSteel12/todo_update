7f2dce6f8e677b5a021f7e45686f8ca3f4dbbf87
AMSteel12 <91567138+AMSteel12@users.noreply.github.com>
2021-10-25 09:03:42 -0500

    Update Login.js

--- src/UserInfo/Login.js
+++ src/UserInfo/Login.js
@@ -1,19 +1,44 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import {useContext} from 'react/cjs/react.development'
+import {StateContext} from '../Contexts'
+import {useResource} from 'react-request-hook';
 
-export default function Login( {dispatchUser} ) {
 
+export default function Login() { 		//{dispatchUser} ) {
+
+	const {dispatch} = useContext(StateContext)
 	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [userLoginFailed, setUserLoginFailed] = useState(false)
 
 	function handleUsername (evt) {setUsername(evt.target.value)}
+	function handlePassword (evt) {setPassword(evt.target.value)}
+
+	const [user, login] = useResource(() => ({
+        url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+        method: 'get'
+    }))
+
+	useEffect(() => {
+        if (user && user.data) {
+            if (user.data.length > 0) {
+                setUserLoginFailed(false);
+                dispatch({type: 'LOGIN', username: user.data[0].username})
+            } else {
+                setUserLoginFailed(true)
+            }
+        } 
+    }, [user]);
 
     return (
-	    <form onSubmit={evt => {evt.preventDefault(); dispatchUser({type:"LOGIN", username});}}>
-	        <label htmlFor="login-userid">User ID:</label>
+					// removed dispatch({type:"LOGIN", username});}}>
+	    <form onSubmit={evt => {evt.preventDefault(); login(); }}> 
+	        <label htmlFor="login">User ID:</label>
 	        <input type="text" value={username} onChange={handleUsername} name="login-userid" id="login-userid" />
 	        <label htmlFor="login-password">Password:</label>
-	        <input type="password" name="login-password" id="login-password" />
-            <input type="submit" value="Login" />
+	        <input type="password" value={password} onChange={handlePassword} name="login-password" id="login-password" />
+            <input type="submit" value="Login" disabled={username.length === 0 || password.length === 0} />
+			{userLoginFailed && <span style={{color: 'crimson'}}> The username and/or password is not valid. Try again. </span>}
 	    </form>
     )
-
 }
