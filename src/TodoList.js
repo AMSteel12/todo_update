2c9b71a30af495eb96ed9681e17a547546f3561d
AMSteel12 <91567138+AMSteel12@users.noreply.github.com>
2021-10-25 09:09:09 -0500

    Update TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import TodoItem from './TodoItem'
+import {StateContext} from './Contexts'
+import {useContext} from 'react/cjs/react.development'
 
-export default function TodoList ({todos = []}) {
+
+export default function TodoList () { //({todos = []}) {
+    const {state} = useContext(StateContext);
+    const {todos} = state;
+
     return (
       <div>
-	 {todos.map((t, i) => <TodoItem {...t} key={'todo-' + i} />)}
+	      {todos.map((t, i) => (<TodoItem key={t.id} {...t} /> ))}
       </div>
     )
 }
