b7e272e1626af0ff87497c6e7246750acb050a0f
AMSteel12 <91567138+AMSteel12@users.noreply.github.com>
2021-10-25 09:08:39 -0500

    Update TodoItem.js

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,27 +1,77 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
+import {useResource} from 'react-request-hook';
+import {ThemeContext, StateContext} from './Contexts'
 
-export default function TodoItem({title, description, creator, complete}) {
+export default function TodoItem({title, description, createdDate, completeStatus, completedDate, id}) {
 
-    const dateCreated = new Date().toDateString();
-    const dateCompleted = new Date().toDateString();
+    // Removed static completed items from HW 2
+   // const dateCreated = new Date().toDateString();
+   // const dateCompleted = new Date().toDateString();
 
+   const {secondaryColor} = useContext(ThemeContext)
+   const {dispatch} = useContext(StateContext)
+
+   const [todo, deleteTodo] = useResource(() => ({
+    url: `/todos/${id}`,
+    method: 'delete',
+  }));
+
+
+  const [editTodo, toggleTodo] = useResource(({completeStatus, completedDate}) => ({
+    url: `/todos/${id}`,
+    method: 'patch',
+    data: {completeStatus, completedDate},
+  }));
+
+
+  useEffect(() => {
+    if (todo && todo.data !== undefined) {
+      dispatch({type: 'DELETE_TODO', id});
+    }
+  }, 
+  [todo]);
+
+  useEffect(() => {
+    if (editTodo && editTodo.data) {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id: editTodo.data.id,
+        completeStatus: editTodo.data.completeStatus,
+        completedDate: editTodo.data.completedDate,
+      });
+    }
+  }, 
+  [editTodo]);
+
+
+  function handleDeleteTodo () {
+	deleteTodo();
+}
+
+   function handleCheckedItem (evt) {
+      let dateTimeHolder = null;
+
+      if (evt.target.checked) {
+        dateTimeHolder = Date.now();
+      }
+      else {
+        dateTimeHolder = null;
+      }
+      toggleTodo({completeStatus: evt.target.checked, completedDate: dateTimeHolder})
+   }
+
+
+	//Updated from HW 3 with adding TOGGLE_TODO and DELETE_TODO
     return (
 	    <div>
-	        <h3>Title: {title}</h3>
+          <h3 style={{color: secondaryColor}}>Title: {title}</h3>
 	          <br />
+            <span> <input type="checkbox" checked={completeStatus} onChange={handleCheckedItem} /> </span>
 	        <div>{description}</div>
 	          <br />
-			<div>Created by: {creator}</div>
 			   <br />  
-	        <i>Date created: </i>
-	        	<br />
-	        <div>{dateCreated}</div>
-	          <br />
-                <div>{complete}</div>
-	          <br />
-	        <i>Date completed: </i>
-	        <div>{dateCompleted}</div>
-	          <br />
+			   <button type="button" onClick={handleDeleteTodo}>Delete</button>
+			   <hr/>
 	          <br />   
 	    </div>   )
 }
