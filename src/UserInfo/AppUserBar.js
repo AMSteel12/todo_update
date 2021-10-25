6df940aa973aa5db9ecfda40497774b44a7406f3
AMSteel12 <91567138+AMSteel12@users.noreply.github.com>
2021-10-25 09:02:56 -0500

    Update AppUserBar.js

--- src/UserInfo/AppUserBar.js
+++ src/UserInfo/AppUserBar.js
@@ -1,28 +1,28 @@
-import React /*,{useState}*/ from 'react'
+import React, {useContext} from 'react'  		//remove useState
 import Logout from './Logout'
 import Login from './Login'
 import RegisterNewUser from './RegisterNewUser'
+import {StateContext} from '../Contexts'
 
-export default function AppUserBar({user, dispatchUser}) {
+export default function AppUserBar(){  //user, dispatchUser}) {
 
 	//const [user, setUser] = useState('')
-
     //const username = ''
     //const username ='Aaron Min'
 
-    if (user) {
-		return <Logout user={user} dispatchUser={dispatchUser}/>
+    const {state} = useContext(StateContext)
+
+    if (state.user) {
+		return <Logout /> 		
     }
 
     else {
 		return (
 	    	<>
-			  <Login dispatchUser={dispatchUser}/>
-			  <RegisterNewUser dispatchUser={dispatchUser}/>
+			  <Login />					
+			  <RegisterNewUser />	
 			</>
 		)
     }
 }
-	
-    
     
